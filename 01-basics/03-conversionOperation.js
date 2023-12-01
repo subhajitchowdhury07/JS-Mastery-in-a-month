53cfe4fca565d8217cae8dea7f72999173963241
subhajit chowdhury <125207665+subhajitchowdhury07@users.noreply.github.com>
2023-12-01 16:11:20 +0000

    conversion of datatypes

---
+++ 01-basics/03-conversionOperation.js
@@ -0,0 +1,29 @@
+let score ="53";
+let score_2nd ="34abc"
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+let value = Number(score_2nd);
+console.log(typeof value);
+console.log(value);
+
+
+// "33" => number
+// "33abc" => NaN
+// "true" => 1
+// "hello" => NaN
+
+let logIn = "";
+let logInBoolean = Boolean(logIn);
+console.log(logInBoolean);
+
+// 1 or more than 1 => true
+// 0 or "" => false
+// "sdsdsd" => false
+
+let sNumber = null;
+let numberAsString = String(sNumber);
+console.log(numberAsString);
+console.log(typeof numberAsString);
