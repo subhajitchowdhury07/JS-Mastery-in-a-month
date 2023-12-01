af9419ee3d7ca1c3bfca58d8c35d420d6141a9bd
subhajit chowdhury <125207665+subhajitchowdhury07@users.noreply.github.com>
2023-12-01 15:49:02 +0000

    datatypes

---
+++ 01-basics/02_datatypes.js
@@ -0,0 +1,9 @@
+"use strict" // treat all the code as  a new version of code 
+
+//alert(3 + 3); // currently we are not using any browser we are using node
+
+let age=9;
+let number = null;
+let x= false;
+let y;
+let string="how r you :)"
