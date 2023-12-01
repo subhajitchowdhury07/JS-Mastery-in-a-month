326f7910af1b1cc5ed88dbfdac7472f5b375c0c3
subhajit chowdhury <125207665+subhajitchowdhury07@users.noreply.github.com>
2023-12-01 15:30:29 +0000

    container config done

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,14 @@
+const accountId =199387;
+let accountName= "how are you";
+var accountType="savings";
+account =233927;
+let y;
+
+accountType="current"
+accountName= "what is your problem"
+
+
+// console.log(accountType);
+// console.log(accountName);
+
+console.table([accountId, accountName , account,y])
